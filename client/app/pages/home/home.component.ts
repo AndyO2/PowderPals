788611750ded7266d43bc132a563184d00f89df6
andyokamoto <andy.okamoto@gmail.com>
2024-10-25 10:46:07 -0700

    more

--- client/app/pages/home/home.component.ts
+++ client/app/pages/home/home.component.ts
@@ -3,20 +3,65 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  filterByContinent(value: Event) {
+    // TODO: Implement resort api to filter by continent
+    console.log(value);
+  }
+  filterByCountry(value: Event) {
+    // TODO: Implement resort api to filter by country
+    console.log(value);
+  }
+
+  filterByCity(value: Event) {
+    // TODO: Implement resort api to filter by city
+    console.log(value);
+  }
 
   isChecked = false;
 
   resorts = [
-    { name: 'Brighton Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
-    { name: 'Alta Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
-    { name: 'Snowbird Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
-    { name: 'Deer Valley Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
-    { name: 'Solitude Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
-    { name: 'Brighton Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
-    { name: 'Brighton Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
-    { name: 'Brighton Ski Resort', description: 'Description 1', image: '../../assets/download.jpeg' },
+    {
+      name: 'Brighton Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
+    {
+      name: 'Alta Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
+    {
+      name: 'Snowbird Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
+    {
+      name: 'Deer Valley Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
+    {
+      name: 'Solitude Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
+    {
+      name: 'Brighton Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
+    {
+      name: 'Brighton Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
+    {
+      name: 'Brighton Ski Resort',
+      description: 'Description 1',
+      image: '../../assets/download.jpeg',
+    },
   ];
 }
